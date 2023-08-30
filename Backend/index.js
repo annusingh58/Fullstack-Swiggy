05977e321c4174be05c1da0fb36050c6e8274da9
anu singh <annusingh58@gmail.com>
2023-08-30 06:48:15 +0530

    updates

--- Backend/index.js
+++ Backend/index.js
@@ -3,5 +3,18 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from "./Route/Userroutes.js";
 
-const
+
+const app=express();
+dotenv.config();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.use('/swiggy',router);
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log("db connected"))
+.catch((err)=>console.log("error",err));
+
+app.listen(process.env.PORT,()=>console.log(`working on  ${process.env.PORT}`))
+
